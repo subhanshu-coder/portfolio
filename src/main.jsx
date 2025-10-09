2a5ab5598d1f7cb151b0ced79820da8e9436740d
subhanshu-pal <subhanshupal7@gmail.com>
2025-10-10 02:46:58 +0530

    Add new navbar component and updated styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // Optional: your global styles
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/3dportfolio">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
