9cc44293b72eaa6a03712925473830078797a08a
subhanshu-pal <subhanshupal7@gmail.com>
2025-10-10 03:11:39 +0530

    Add new navbar component and updated styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,7 @@
+import React from 'react'; // Often good practice to import React
+import ReactDOM from 'react-dom/client'; // <--- **Add this line**
 import { BrowserRouter } from 'react-router-dom';
+import App from './App'; // Assuming App is in './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
