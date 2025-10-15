02366c3db6675067ad9d1432280bf015c8a01088
subhanshu-pal <subhanshupal7@gmail.com>
2025-10-15 23:25:33 +0530

    Update navbar styles to box layout and modify Navbar component

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/3dportfolio/',
-  plugins: [react()]
+  plugins: [react()],
+  // Set the base public path to the name of your deployment subdirectory
+  base: '/3dportfolio/', 
 });
